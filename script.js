bc59b682cd3530f41f411374e18a6b56da21f57d
Bridget Shannon <bridgetrshannon@gmail.com>
2020-09-18 14:19:34 -0400

    trying to fix display schedule function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var hr = [];
 
 var blockHour;
 
-var scheduleInput;
+// var scheduleInput;
 
 // 1. Display current time with Moment.js
 $("#currentDay").append(moment().format("dddd, MMMM Do h:mm a"));
@@ -15,54 +15,30 @@ $("#currentDay").append(moment().format("dddd, MMMM Do h:mm a"));
 $(".saveBtn").on("click", saveSchedule)
 
 function saveSchedule() {
-        // // inside function
-        // use jquery to get button value
-        // var timeText = $(this).val();
-        // localStorage.setItem(timeText, "text")
-
-        //      example
-        // localStorage.getItem("hr9")
-        // console.log(localStorage.getItem("hr9"))
 
         const $button = $(this);
         const time = $button.val();
         const text = $button.siblings("textarea").val();
 
-        // localStorage.setItem(time, "textarea")
-
         localStorage.setItem(time, text);
+
         // use jquery to grab text from previous sibling / element var timeText = $(this).val(); (not val though, something else)
 }
 
-// happens outside of function 
-// localStorage.getItem("")
-// then put on the page - hook an element using jquery 
-
-
 // 3. When app opens, display content for each time block from local storage.
 // can be done with a loop (better) or by targeting each set block separately
-// localstorage.getItem 
-
-// array that holds the hour times from 9 am - 5 pm
-var hourArr = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 function displaySchedule() {
 
-        for (let i = 0; i < hourArr.length; i++)
-                console.log(hourArr[i]);
+        // for (let i = 0; i < hourArr.length; i++)
+        //         console.log(hourArr[i]);
 
-
-        // var scheduleInput = document.getElementById("")
+        // localStorage.getItem(time, text);
+        // append
 }
 displaySchedule();
 
-// IDK What this is 
-//         localStorage.setItem( 'car', JSON.stringify(car) );
-// console.log( JSON.parse( localStorage.getItem( 'car' ) ) );
-
 // 4. Change time block background color depending in the current hour (past = grey , present = red, future = green)
-
-// console.log(moment().hour());
 
 function timeCheck() {
         $(".hour").each(function () {
